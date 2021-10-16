508b66485ac029de6b1e4086e9ceebb7cf7fc2c0
mikiiwai1990 <91274738+mikiiwai1990@users.noreply.github.com>
2021-10-16 18:13:12 +0900

    修正

--- test3.php
+++ test3.php
@@ -1,12 +1,30 @@
 <?php
-//5次元配列
-$fruit =['apple', 'grape'];
-$origin = ['青森', '山梨',];
-$stock = [2, 3,];
-$price = [120, 200,];
+// 連想配列
+$box=['fruit'=>['apple','grape']];
+echo $box['fruit'][1];
 
-$box = [$fruit, $origin, $stock, $price];
+echo '<br>';
 
-var_dump($box[0][0]) ;
+//2次元配列?
+$box1=[['apple','grape'],['青森','山梨']];
+
+echo $box1[1][0];
+
+echo '<br>';
+
+//5次元配列？
+$box1=[['apple'=>['青森'=>[2=>[120]]],'grape'=>['山梨'=>[3=>[200]]]]];
+
+echo $box[0][0];
+
+// $fruit =['apple', 'grape'];
+// $origin = ['青森', '山梨',];
+// $stock = [2, 3,];
+// $price = [120, 200,];
+
+// $fruit =['origin'=>'stock'=>[price]];
+// $box = [$fruit, $origin, $stock, $price];
+
+// var_dump($box[0][0]) ;
 
 ?>
